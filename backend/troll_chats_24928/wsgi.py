ba8e2061bf21adfb6662785eb2380e44439a550a
Crowdbotics <team@crowdbotics.com>
2021-03-09 10:52:06 +0000

    Update models

--- backend/troll_chats_24928/wsgi.py
+++ backend/troll_chats_24928/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'troll_chats_24928.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "troll_chats_24928.settings")
 
 application = get_wsgi_application()
